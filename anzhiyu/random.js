a0806a37bfa994e9012fcd329a9e7b4db0f39b49
domf <413085643@qq.com>
2024-09-14 15:35:33 +0800

    Site updated: 2024-09-14 15:35:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/13/FreeModbus/","2024/09/13/STM32F407-modbus/","2024/09/14/ringfifo-0/"];function toRandomPost(){
+var posts=["2024/09/13/FreeModbus/","2024/09/14/ringfifo-0/","2024/09/13/STM32F407-modbus/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
