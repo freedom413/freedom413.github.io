d0d4892e45d3c53b53b1438533ca055f9aeb2d72
domf <413085643@qq.com>
2024-09-19 19:01:35 +0800

    Site updated: 2024-09-19 19:01:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/13/FreeModbus/","2024/09/13/STM32F407-modbus/","2024/09/14/miniroot/","2024/09/14/ringfifo-0/"];function toRandomPost(){
+var posts=["2024/09/13/STM32F407-modbus/","2024/09/10/FreeModbus/","2024/09/06/miniroot/","2024/09/01/ringfifo-0/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
