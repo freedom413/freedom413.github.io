d5fe0634745e4a1862ae9174fd5524349850ce00
freedom413 <413085643@qq.com>
2024-09-23 17:45:51 +0800

    Site updated: 2024-09-23 17:45:51

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/10/FreeModbus/","2024/09/13/STM32F407-modbus/","2024/09/22/freertos-1/","2024/09/23/freertos-2/","2024/09/23/freertos-3/","2024/09/06/miniroot/","2024/09/01/ringfifo-0/"];function toRandomPost(){
+var posts=["2024/09/10/FreeModbus/","2024/09/22/freertos-1/","2024/09/13/STM32F407-modbus/","2024/09/23/freertos-3/","2024/09/23/freertos-2/","2024/09/06/miniroot/","2024/09/01/ringfifo-0/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
